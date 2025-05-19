d6bbab8f9f77057248475141c119db04ecbe8e76
lm-hotton <louismaxence.hotton@gmail.com>
2025-05-19 21:07:31 +0200

    code fonction pour 1 choc

--- test.py
+++ test.py
@@ -4,64 +4,43 @@ import plotly.express as px
 import pandas as pd
 import endaq
 import matplotlib.pyplot as plt
+ 
+#Paramètres du choc
+duree=1
+fe=400000
+fc=30000
+nbr_chocs=3
+lambdaa=10
+ 
+choc=[]
 
+#Création des chocs
+t=np.linspace(0,duree,duree*fe)
+expo=1*np.exp(-lambdaa*t)
+b, a = scipy.signal.butter(8, fc/fe)
+for i in range(nbr_chocs):
+    random=np.random.normal(loc=0, scale=1, size=duree*fe)
+    y=random*expo
+    y[0]=0
+    y = scipy.signal.filtfilt(b, a, y, padlen=150)
+    choc.append(y)
+ 
+#Affichage des chocs
+for i in range(nbr_chocs):
+    plt.plot(t,choc[i])
+plt.show()
 
-t=np.linspace(0,1,1*400000)
-random=np.random.uniform(1,-1,1*400000)
-
-fig0 = px.line(
-        t,
-        x=t,
-        y=random,
-    )
-#fig0.show()
-
-
-#Calculate PSD
-f, psd = scipy.signal.welch(t,fs=400000.,window='hann', nperseg=32768,noverlap=0.5)
-
-#Plot PSD
-fig1 = px.line(f,
-               x=f,
-               y=psd,
-               log_y=True
-    )
-#fig1.show()
-
-expo=1*np.exp(-10*t)
-choc=random*expo
-
-fig2 = px.line(
-        t,
-        x=t,
-        y=choc,
-    )
-#fig2.show()
-
-b, a = scipy.signal.butter(8, 10000/400000)
-choc2 = scipy.signal.filtfilt(b, a, choc, padlen=150)
-
-fig3 = px.line(
-        t,
-        x=t,
-        y=choc2,
-    )
-fig3.show()
-
-pdchoc2=pd.DataFrame(choc2,t)
-print(pdchoc2)
-
-#Calculate SRS
-freqs = endaq.calc.utils.logfreqs(pdchoc2, init_freq=1, bins_per_octave=12)
-srs = endaq.calc.shock.shock_spectrum(pdchoc2, freqs=freqs, damp=10., mode='srs')
-
+#f,dsp=scipy.signal.welch(choc[1], fs=fe, window='hann', nperseg=None, noverlap=0.5, nfft=None, detrend='constant', return_onesided=True, scaling='density', axis=-1, average='mean')
+#plt.loglog(f,dsp)
+#plt.show()
+ 
+#Calcul de SRS
+srs=[]
+freqs = np.logspace(start=np.log10(1),stop=np.log10(50000), num=1000, endpoint=True, base=10)
+for i in range(nbr_chocs):
+    srs.append(endaq.calc.shock.shock_spectrum(pd.DataFrame(choc[i],t), freqs=freqs, damp=0.05, mode='srs'))
+ 
 #Plot SRS
-fig4 = px.line(srs).update_layout(
-    title_text='Shock Response Spectrum (SRS) of Motorcycle Crash',
-    xaxis_title_text="Natural Frequency (Hz)",
-    yaxis_title_text="Peak Acceleration (g)",
-    legend_title_text='',
-    xaxis_type="log",
-    yaxis_type="log",
-  )
-fig4.show()
+for i in range(nbr_chocs):
+    plt.loglog(freqs,srs[i])
+plt.show()
