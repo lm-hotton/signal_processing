5cfef4de70fc81e44b4c2956986fb423fe333de8
lm-hotton <louismaxence.hotton@gmail.com>
2025-05-19 21:58:26 +0200

    Ajout notebook

--- test.py
+++ test.py
@@ -8,39 +8,34 @@ import matplotlib.pyplot as plt
 #Paramètres du choc
 duree=1
 fe=400000
-fc=30000
+fc=20000
 nbr_chocs=3
 lambdaa=10
  
-choc=[]
 
 #Création des chocs
 t=np.linspace(0,duree,duree*fe)
 expo=1*np.exp(-lambdaa*t)
 b, a = scipy.signal.butter(8, fc/fe)
-for i in range(nbr_chocs):
-    random=np.random.normal(loc=0, scale=1, size=duree*fe)
-    y=random*expo
-    y[0]=0
-    y = scipy.signal.filtfilt(b, a, y, padlen=150)
-    choc.append(y)
- 
-#Affichage des chocs
-for i in range(nbr_chocs):
-    plt.plot(t,choc[i])
+random=np.random.normal(loc=0, scale=1, size=duree*fe)
+y=random*expo
+y[0]=0
+y = scipy.signal.filtfilt(b, a, y, padlen=150)
+choc=y
+
+
+#Decimation
+choc_decim=[]
+t_decim = []
+for q in range(11):
+    choc_decim.append(scipy.signal.decimate(choc, (q+1)))
+    t_decim.append(np.linspace(0, duree, len(choc_decim), endpoint=False))
+    plt.plot(t_decim[q],choc_decim[q])
 plt.show()
 
-#f,dsp=scipy.signal.welch(choc[1], fs=fe, window='hann', nperseg=None, noverlap=0.5, nfft=None, detrend='constant', return_onesided=True, scaling='density', axis=-1, average='mean')
-#plt.loglog(f,dsp)
-#plt.show()
- 
-#Calcul de SRS
-srs=[]
+src=[]
 freqs = np.logspace(start=np.log10(1),stop=np.log10(50000), num=1000, endpoint=True, base=10)
-for i in range(nbr_chocs):
-    srs.append(endaq.calc.shock.shock_spectrum(pd.DataFrame(choc[i],t), freqs=freqs, damp=0.05, mode='srs'))
- 
-#Plot SRS
-for i in range(nbr_chocs):
-    plt.loglog(freqs,srs[i])
+for q in range(11):
+    src.append(endaq.calc.shock.shock_spectrum(pd.DataFrame(choc,t), freqs=freqs, damp=0.05, mode='srs'))
+    plt.loglog(freqs,src[q-1])
 plt.show()
